bd8f44df1da7fd854641e1ced066954b74ec5b8a
Senan Shahverdiyev <senanshahverdiyev2004@gmail.com>
2024-12-15 19:34:57 +0400

    update

---
+++ js/commands.js
@@ -0,0 +1,99 @@
+var linkedin = "https://www.linkedin.com/in/sanan-shahverdiyev/";
+var instagram = "https://www.instagram.com/ssn44n/";
+var github = "https://github.com/sananshahverdiyev";
+var email = 'mailto:sananshahverdiyev.work@gmail.com';
+var medium=  'https://medium.com/@ssnaan';
+var resume= 'img/Sanan Shahverdiyev CV.pdf';
+;
+whois = [
+  "<br>",
+  "I am Sanan Shahverdiyev, an IT professional with a strong foundation in networking, troubleshooting, ",
+  "and system management. I hold industry-recognized certifications, including the Palo Alto Certified ",
+  "Network Security Administrator (PCNSA), Cisco Certified Network Associate (CCNA), and Certified ",
+  "Kubernetes Administrator (CKA), showcasing my expertise in modern IT and cloud technologies. ",
+  "My professional journey includes experience as a Support Specialist at AzEduNet and an IT Specialist ",
+  "at One Degree LTD, where I developed skills in problem-solving, hardware and software management, and ",
+  "delivering high-quality technical support. Additionally, I have gained hands-on experience as a DevOps ",
+  "Intern at Rateingale and AistGroup, where I worked on Kubernetes deployments, CI/CD pipelines, and ",
+  "cloud infrastructure optimization. ",
+  "Currently, I am expanding my knowledge in DevOps and cloud computing, focusing on tools like Docker, ",
+  "Kubernetes, Terraform, and CI/CD platforms. I have successfully configured automation workflows, SSL ",
+  "certificate management, and scalable deployments in Kubernetes environments. My goal is to advance my ",
+  "career as a DevOps and DevSecOps Engineer by integrating best practises on those fields. ",
+  "In addition to my technical skills, I have completed courses in Network and Cybersecurity at STEP IT ",
+  "Academy and hold a BBA in Computer Engineering from Azerbaijan State Oil and Industry University. ",
+  "Beyond my professional pursuits, I enjoy sports and personal development, striving for continuous ",
+  "growth and balance in all areas of life. ",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: Am I an Genius or an idoit.",
+  "<br>"
+];
+
+
+projects = [
+  "<br>",
+  "Although most of my projects are confidental you can still check my github to see some of them",
+  "You can also use the following to connect with me or look at some blogs",
+  '<span class="command">linkedin</span>        Connect with me',
+  '<span class="command">github</span>          view my codes ',
+  '<span class="command">medium</span>          Read more stuff ',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">Whois</span>           Who is Sanan?',
+  '<span class="command">Banner</span>          Display the header,',
+  '<span class="command">Clear</span>           Clear terminal',
+  '<span class="command">Contact</span>         spam or call',
+  '<span class="command">Help</span>            You obviously already know what this does',
+  '<span class="command">History</span>         View command history',
+  '<span class="command">Projects</span>        View coding projects',
+  '<span class="command">Resume</span>          download resume',
+  '<span class="command">Socials</span>         Connect and Read my stuff ',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Sanan Shahverdiyev All rights reserved © :).</span>',
+"  ██████  ▄▄▄       ███▄    █  ▄▄▄       ███▄    █                                                   ",                                                   
+"▒██    ▒ ▒████▄     ██ ▀█   █ ▒████▄     ██ ▀█   █                                                   ",     
+"░ ▓██▄   ▒██  ▀█▄  ▓██  ▀█ ██▒▒██  ▀█▄  ▓██  ▀█ ██▒                                                  ",    
+"  ▒   ██▒░██▄▄▄▄██ ▓██▒  ▐▌██▒░██▄▄▄▄██ ▓██▒  ▐▌██▒                                                  ",     
+"▒██████▒▒ ▓█   ▓██▒▒██░   ▓██░ ▓█   ▓██▒▒██░   ▓██░                                                  ",    
+"▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒   ▓▒█░░ ▒░   ▒ ▒                                                   ",   
+"░ ░▒  ░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░  ▒   ▒▒ ░░ ░░   ░ ▒░                                                  ",    
+"░  ░  ░    ░   ▒      ░   ░ ░   ░   ▒      ░   ░ ░                                                   ", 
+"      ░        ░  ░         ░       ░  ░         ░                                                   ",  
+"  ██████  ██░ ██  ▄▄▄       ██░ ██  ██▒   █▓▓█████  ██▀███  ▓█████▄  ██▓▓██   ██▓▓█████ ██▒   █▓     ",    
+"▒██    ▒ ▓██░ ██▒▒████▄    ▓██░ ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒▒██▀ ██▌▓██▒ ▒██  ██▒▓█   ▀▓██░   █▒     ",    
+"░ ▓██▄   ▒██▀▀██░▒██  ▀█▄  ▒██▀▀██░ ▓██  █▒░▒███   ▓██ ░▄█ ▒░██   █▌▒██▒  ▒██ ██░▒███   ▓██  █▒░     ",       
+"  ▒   ██▒░▓█ ░██ ░██▄▄▄▄██ ░▓█ ░██   ▒██ █░░▒▓█  ▄ ▒██▀▀█▄  ░▓█▄   ▌░██░  ░ ▐██▓░▒▓█  ▄  ▒██ █░░     ",         
+"▒██████▒▒░▓█▒░██▓ ▓█   ▓██▒░▓█▒░██▓   ▒▀█░  ░▒████▒░██▓ ▒██▒░▒████▓ ░██░  ░ ██▒▓░░▒████▒  ▒▀█░       ",          
+"▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒ ▒▒   ▓▒█░ ▒ ░░▒░▒   ░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░ ▒▒▓  ▒ ░▓     ██▒▒▒ ░░ ▒░ ░  ░ ▐░       ",      
+"░ ░▒  ░ ░ ▒ ░▒░ ░  ▒   ▒▒ ░ ▒ ░▒░ ░   ░ ░░   ░ ░  ░  ░▒ ░ ▒░ ░ ▒  ▒  ▒ ░ ▓██ ░▒░  ░ ░  ░  ░ ░░       ",        
+"░  ░  ░   ░  ░░ ░  ░   ▒    ░  ░░ ░     ░░     ░     ░░   ░  ░ ░  ░  ▒ ░ ▒ ▒ ░░     ░       ░░       ",               
+"      ░   ░  ░  ░      ░  ░ ░  ░  ░      ░     ░  ░   ░        ░     ░   ░ ░        ░  ░     ░       ",                  
+"                                        ░                    ░           ░ ░                ░        ",    
+"                                                                                                     ",
+  '<span class="color2">Welcome to my portfolio.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+socials =[
+  "<br>",
+  '<span class="command">linkedin</span>        Connect with me',
+  '<span class="command">github</span>          view my codes ',
+  '<span class="command">medium</span>          Reead More Stuff ',
+"<br>",
+];
+ 
+contact = [
+     "<br>",
+  " Use the email command to open email window ",
+  " Phone: +994508664114 ",
+ ];
